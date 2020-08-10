68ae60701c2a2d1838bb4e42044107d37f8a7ee3
dollarmatian <nathansalandy@gmail.com>
2020-08-10 13:40:31 -0400

    server redirect and main styling

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,136 @@
 const functions = require('firebase-functions');
+const { error } = require('firebase-functions/lib/logger');
+const express = require('express');
+const fetch = require('node-fetch');
+const { response, json } = require('express');
+const { query } = require('express');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
+
+const app = express()
+
+
+app.get("/openweather", (req,res)=>{
+    // res.send("Hello World")
+    //  const city = req.query.q
+  const request = req.query.q
+    // console.log(req.params)
+  fetch(`https://api.openweathermap.org/data/2.5/weather?${req.query.q}&units=metric&APPID=${functions.config().openweather.key}`)
+    .then(res => res.json()) 
+       .then(json => res.send(json) )
+
+    
+    .catch((error) => {
+        console.error(error);
+      });  
+
+    })
+
+
+    
+//   app.get('/:place', async (request, response) => {
+//     console.log(request.params)
+//     const city = request.params.place
+//     console.log(city)
+//     const api_key = functions.config().openweather.key;
+//     console.log(api_key)
+//     const weather_url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&APPID=${api_key}`;
+//     console.log(weather_url)
+//     const weather_response = await fetch(weather_url);
+//     const weather_data = await weather_response.json();
+//     response.send(weather_data)
+
+//     .catch((error) => {
+//               console.error(error);
+//             });  
+
+// })
+
+
+
+
+ // Request 1
+ exports.openweather =  functions.https.onRequest(app)
+
+//Request 2
+
+// exports.openweather = functions.https.onRequest((request,response)=>{
+    
+//     const app = express()
+
+
+//     app.get("/:city", async (req,res)=>{
+//         // res.send("Hello World")
+//         const city = req.params.city
+//         console.log(city)
+//         console.log(req.params)
+            
+//        await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&APPID=${functions.config().openweather.key}`)
+//         .then(res => res.json())   
+//         .then(json => res.json(json))
+//         .catch((error) => {
+//             console.error(error);
+//           });  
+//         response.send(app)
+       
+        
+//     })
+    
 // });
+
+
+// exports.OpenWeatherNew =  functions.https.onRequest((request,response)=>{
+//     console.log('started')
+//     const app = express();
+//     console.log('start next')
+//     const port = 3000;
+// app.listen(port, () => {
+//   console.log(`Starting server at ${port}`);
+
+// });
+// console.log('next')
+
+//     let data
+//     console.log('data set')
+//   app.get('/', async (request, response) => {
+//     const api_key = functions.config().openweather.key;
+//     console.log(api_key)
+//     const weather_url = `https://api.openweathermap.org/data/2.5/weather?q=London&units=metric&APPID=${api_key}`;
+//     console.log(weather_url)
+//     const weather_response = await fetch(weather_url);
+    
+
+//     data = response.json('hello');
+
+// })
+// response.send('hello:'+ data)
+// })
+// exports.OpenWeatherTry =  functions.https.onRequest((request,response)=>{
+    
+    // app.get('/', async (request, response) => {
+//     console.log(request.params);
+//     const latlon = request.params.latlon.split(',');
+//     console.log(latlon);
+//     const lat = latlon[0];
+//     const lon = latlon[1];
+//     console.log(lat, lon);
+//     const api_key = process.env.API_KEY;
+//     const weather_url = `https://api.darksky.net/forecast/${api_key}/${lat},${lon}/?units=si`;
+//     const weather_response = await fetch(weather_url);
+//     const weather_data = await weather_response.json();
+  
+//     const aq_url = `https://api.openaq.org/v1/latest?coordinates=${lat},${lon}`;
+//     const aq_response = await fetch(aq_url);
+//     const aq_data = await aq_response.json();
+  
+//     const data = {
+//       weather: weather_data,
+//       air_quality: aq_data
+//     };
+//     response.json(data);
+//   });
+
+        
+    
+      
+    // })
+    
--- src/App.js
+++ src/App.js
@@ -1,11 +1,5 @@
 import React, { useState } from 'react';
 
-const API = {
-  key: "d911abcff80d2344f9d5e0a9ca3d3682",
-  base: "https://api.openweathermap.org/data/2.5/"
-}
-
-
 
 function App() {
   const [query, setQuery] = useState('');
@@ -13,13 +7,16 @@ function App() {
 
   const search = evt => {
     if (evt.key === "Enter") {
-      fetch(`${API.base}weather?q=${query}&units=metric&APPID=${API.key}`)
+      
+      const querySend = `q=${query}`
+      setQuery('');
+      fetch(`https://weather.dollarmatian.dev/openweather?q=${querySend}`)
         .then(res => res.json())
         .then(result => {
           setWeather(result);
 
-          setQuery('');
-          console.log(result)
+          
+          
 
         })
     }
@@ -36,20 +33,24 @@ function App() {
     let year = d.getFullYear();
 
 
-    return ` ${day} ${date} ${month} ${year}`;
+    return `${day}, ${date} ${month} ${year}`;
   }
 
   // let Degrees_F_or_C = 'metric';
   return (
     <div className={(typeof weather.main != "undefined") ? ((weather.main.temp > 16) ? 'app warm' : 'app') : 'app'}>
       <main>
+        
+         
         <div className="search-box">
-          <input type="text" className="search-bar" placeholder="Enter City Name"
+          <h1 className="title">Doggone Weather</h1>
+          <input type="text" className="search-bar" placeholder="Enter City (e.g. London or London,GB)"
             onChange={e => setQuery(e.target.value)}
             value={query}
             onKeyPress={search}
           />
         </div>
+        
 
         {(typeof weather.main != "undefined") ? (
           <div className="info">
@@ -60,7 +61,7 @@ function App() {
             <div className="weather-box">
 
 
-              (<div className="temp">{Number((weather.main.temp)).toFixed(0)}℃</div>
+              <div className="temp">{Number((weather.main.temp)).toFixed(0)}℃</div>
 
               <div className="weather">{weather.weather[0].main}</div>
             </div>
